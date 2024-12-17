17ea8d937afe28b354189ae33a851f95c4c43bda
mortalasd <mortalwebai@gmail.com>
2024-12-17 05:02:32 +0000

    .

---
+++ script.js
@@ -0,0 +1,3 @@
+// Adaptação para usar o canvas criado dinamicamente
+const canvas = document.querySelector('canvas'); // Encontra o único canvas na página
+kF = e(canvas); // Define o contexto do canvas dinamicamente
